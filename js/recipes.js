7f297e77af464372ae4b56dfd68d4adefa619bc5
Jag Reehal <jag.reehal@gmail.com>
2013-06-21 07:27:26 +0100

    Added missing files

---
+++ js/recipes.js
@@ -0,0 +1,421 @@
+define(function () {
+        return [{
+            "Author": "Homer Simpson",
+            "CookTime": "3 minutes",
+            "Description": "Go nuts for doughnuts with this mothwatering recipe.",
+            "FoodistaUrl": "http://www.foodista.com/recipe/MLX24CBP/quick-doughnuts",
+            "Id": 1,
+            "Ingredients": [
+                {
+                    "Text": "Oil (for deep frying)"
+                },
+                {
+                    "Text": "2 cups sifted flour"
+                },
+                {
+                    "Text": "3 tablespoons sugar"
+                },
+                {
+                    "Text": "1 teaspoon nutmeg"
+                },
+                {
+                    "Text": "cup butter"
+                },
+                {
+                    "Text": "1 egg"
+                },
+                {
+                    "Text": "cup milk"
+                },
+                {
+                    "Text": "1/2 cup confectioners sugar"
+                }
+            ],
+            "Instructions": [
+                {
+                    "Step": 1,
+                    "Text": "Preheat oil to 375 degrees."
+                },
+                {
+                    "Step": 2,
+                    "Text": "Combine flour, sugar, and nutmeg in bowl."
+                },
+                {
+                    "Step": 3,
+                    "Text": "Cut butter with pastry blender or knives. Set aside."
+                },
+                {
+                    "Step": 4,
+                    "Text": "Beat egg in measuring cup."
+                },
+                {
+                    "Step": 5,
+                    "Text": "Add milk to measure 2/3 cup."
+                },
+                {
+                    "Step": 6,
+                    "Text": "Add to flour mixture."
+                },
+                {
+                    "Step": 7,
+                    "Text": "Stir with fork until blended."
+                },
+                {
+                    "Step": 8,
+                    "Text": "Knead just until smooth."
+                },
+                {
+                    "Step": 9,
+                    "Text": "Roll out to 1/2 inch thickness."
+                },
+                {
+                    "Step": 10,
+                    "Text": "Cut out with floured doughnut cutter."
+                },
+                {
+                    "Step": 11,
+                    "Text": "Deep fry 3 to 4 minutes."
+                },
+                {
+                    "Step": 12,
+                    "Text": "Drain on paper towel."
+                },
+                {
+                    "Step": 13,
+                    "Text": "Place confectioners sugar in a bag."
+                },
+                {
+                    "Step": 14,
+                    "Text": "Shake 2 doughnuts at a time to coat doughnuts."
+                }
+            ],
+            "LargeImage": "images/doughnuts_128.png",
+            "Name": "Doughnuts",
+            "NutritionalFacts": [
+                {
+                    "Name": "calories",
+                    "Text": "240 calories"
+                },
+                {
+                    "Name": "fatContent",
+                    "Text": "9g fat"
+                }
+            ],
+            "PrepTime": "15 minutes",
+            "Thumbnail": "images/doughnuts_48.png",
+            "Yield": "6 doughnuts"
+        },
+        {
+            "Author": "Sheri Wetherell",
+            "CookTime": "55 minutes",
+            "Description": "Perfect for autumn when pumpkin is in season. This classic American recipe is sure to be a family favourite.",
+            "FoodistaUrl": "http://www.foodista.com/recipe/85C3ZDPN/classic-pumpkin-pie",
+            "Id": 2,
+            "Ingredients": [
+                {
+                    "Text": "1 (15 ounce) can pumpkin"
+                },
+                {
+                    "Text": "1 (14 ounce) can Sweetened Condensed Milk"
+                },
+                {
+                    "Text": "2 large eggs"
+                },
+                {
+                    "Text": "1 teaspoon ground cinnamon"
+                },
+                {
+                    "Text": "1/2 teaspoon ground ginger"
+                },
+                {
+                    "Text": "1/2 teaspoon ground nutmeg"
+                },
+                {
+                    "Text": "1/2 teaspoon salt"
+                },
+                {
+                    "Text": "1 (9 inch) unbaked pie crust"
+                },
+                {
+                    "Text": "Whipped cream for garnish"
+                }
+            ],
+            "Instructions": [
+                {
+                    "Step": 1,
+                    "Text": "Preheat oven to 425 degrees F."
+                },
+                {
+                    "Step": 2,
+                    "Text": "Whisk pumpkin, sweetened condensed milk, eggs, spices and salt in medium bowl until smooth. Pour into crust. Bake 15 minutes."
+                },
+                {
+                    "Step": 3,
+                    "Text": "Reduce oven temperature to 350 degrees F and continue baking 35 to 40 minutes or until knife inserted 1 inch from crust comes out clean. Cool."
+                },
+                {
+                    "Step": 4,
+                    "Text": "Garnish with whipped cream."
+                }
+            ],
+            "LargeImage": "images/pumpkinpie_128.png",
+            "Name": "Pumpkin Pie",
+            "NutritionalFacts": [
+                {
+                    "Name": "calories",
+                    "Text": "240 calories"
+                },
+                {
+                    "Name": "fatContent",
+                    "Text": "9g fat"
+                }
+            ],
+            "PrepTime": "15 minutes",
+            "Thumbnail": "images/pumpkinpie_48.png",
+            "Yield": "1 pie"
+        },
+        {
+            "Author": "Swiss Tony",
+            "CookTime": "20 minutes",
+            "Description": "A timeless dessert. Lavish the sponge with your choice of cream and jam.",
+            "FoodistaUrl": "http://www.foodista.com/recipe/4RGT4QP5/swiss-roll",
+            "Id": 3,
+            "Ingredients": [
+                {
+                    "Text": "1 cup flour, self-raising"
+                },
+                {
+                    "Text": "3 eggs"
+                },
+                {
+                    "Text": "cup sugar (caster)"
+                },
+                {
+                    "Text": "3 tablespoons boiling water"
+                },
+                {
+                    "Text": "cup raspberry jam"
+                },
+                {
+                    "Text": "sugar for dredging"
+                }
+            ],
+            "Instructions": [
+                {
+                    "Step": 1,
+                    "Text": "Grease a 30cm X 25cm swiss roll tin, line with greaseproof paper and brush with melted butter. Set oven temperature to moderately hot (360F)."
+                },
+                {
+                    "Step": 2,
+                    "Text": "Seperate Eggs."
+                },
+                {
+                    "Step": 3,
+                    "Text": "In a clean bowl, beat egg whites until stiff, gradually add caster sugar, beating until sugar is dissolved and mixture is thick and glossy."
+                },
+                {
+                    "Step": 4,
+                    "Text": "Add yolks and beat until combined. Sift all dry ingredients together, and gently fold into mixture. Spoon boiling water around edge of bowl and fold in."
+                },
+                {
+                    "Step": 5,
+                    "Text": "Pour into prepared tin and shake gently into the corners. Bake for 10 minutes or until the cake springs back when lightly tapped with your finger."
+                },
+                {
+                    "Step": 6,
+                    "Text": "Place jam in a saucepan and heat until it bubbles."
+                },
+                {
+                    "Step": 7,
+                    "Text": "Have ready a sheet of greaseproof paper generously sprinkled with sugar and a sharp knife. Immediately when cake is cooked turn out onto the sugared paper."
+                },
+                {
+                    "Step": 8,
+                    "Text": "Peel of the lining paper and cut off the crisp edges. Grasp the edge of the sugared paper and quickly roll up the cake in the paper, then unroll. Spread with the warm jam to within 2.5cm of the edges."
+                },
+                {
+                    "Step": 9,
+                    "Text": "Reroll this time without the paper and sprinkle with more sugar if necessary. Stand on a cake cooler with the seam underneath until cold."
+                }
+            ],
+            "LargeImage": "images/swissroll_128.png",
+            "Name": "Swiss Roll",
+            "NutritionalFacts": [
+                {
+                    "Name": "calories",
+                    "Text": "240 calories"
+                },
+                {
+                    "Name": "fatContent",
+                    "Text": "9g fat"
+                }
+            ],
+            "PrepTime": "15 minutes",
+            "Thumbnail": "images/swissroll_48.png",
+            "Yield": "1 roll"
+        },
+        {
+            "Author": "Helen Pitlick",
+            "CookTime": "20 minutes",
+            "Description": "Flaky and flavourful, these croissants will taste just like those from your local French bakery.",
+            "FoodistaUrl": "http://www.foodista.com/recipe/BNQT5Z6V/granary-croissants",
+            "Id": 4,
+            "Ingredients": [
+                {
+                    "Text": "150 ml lukewarm water ( 1/2 cup)"
+                },
+                {
+                    "Text": "200 grams unsweetened partly-skimmed evaporated milk warmed (7oz)"
+                },
+                {
+                    "Text": "25 grams (1oz) Fresh yeast or 15g ( ½ oz) dried yeast"
+                },
+                {
+                    "Text": "50 grams unsalted butter, melted (2oz)"
+                },
+                {
+                    "Text": "500 grams granary flour (1lb)"
+                },
+                {
+                    "Text": "a pinch of salt"
+                },
+                {
+                    "Text": "75 grams margaine (3oz)"
+                },
+                {
+                    "Text": "milk, to glaze"
+                }
+            ],
+            "Instructions": [
+                {
+                    "Step": 1,
+                    "Text": "Combine the water with the evaporated milk, and then crumble in the fresh yeast, or stir in the dried yeast. Add the butter. Sift the flour with the salt in a large bowl, returning the grains from the sieve to the flour in the bowl. Rub the margarine into the flour until the mixture resembles breadcrumbs."
+                },
+                {
+                    "Step": 2,
+                    "Text": "Make a well in the center of the flour, pour in the yeast mixture and mix thoroughly. Turn the dough on to a lightly floured surface and knead for 3 minutes. Return the dough to the bowl, cover with a damp tea towel and leave to rise in a warm place for approximately 30 minutes until doubled in size."
+                },
+                {
+                    "Step": 3,
+                    "Text": "Turn half of the risen dough on to a lightly floured surface and roll into a circle about 5mm ( 1/4 inch) thick. Using a sharp knife, cut the dough into eight triangular segments. Working from the outside edge, roll each segment into the middle. Bend each piece into a crescent and place on a lightly oiled baking sheet. Cover with a tea towel and leave to double in size."
+                },
+                {
+                    "Step": 4,
+                    "Text": "Meanwhile preheat the oven to Gas Mark 5/190 C/375 F. Repeat the shaping process with the other half of the dough. Alternatively, leave the remaining dough covered in the refrigerator for up to 4 days and use when fresh croissants are required."
+                },
+                {
+                    "Step": 5,
+                    "Text": "When the croissants have doubled in size, glaze them with the milk and bake in the oven for 15-20 minutes until puffed and golden."
+                }
+            ],
+            "LargeImage": "images/croissants_128.png",
+            "Name": "Croissants",
+            "NutritionalFacts": [
+                {
+                    "Name": "calories",
+                    "Text": "240 calories"
+                },
+                {
+                    "Name": "fatContent",
+                    "Text": "9g fat"
+                }
+            ],
+            "PrepTime": "30 minutes",
+            "Thumbnail": "images/croissants_48.png",
+            "Yield": "6 croissants"
+        },
+        {
+            "Author": "Lori Zappala",
+            "CookTime": "20 minutes",
+            "Description": "These cupcakes are perfect for any occasion.",
+            "FoodistaUrl": "http://www.foodista.com/recipe/LZMRCDCH/cherry-coconut-cupcakes",
+            "Id": 5,
+            "Ingredients": [
+                {
+                    "Text": "3/4 cup butter, softened"
+                },
+                {
+                    "Text": "1 1/2 cups granulated sugar"
+                },
+                {
+                    "Text": "3 eggs"
+                },
+                {
+                    "Text": "1 teaspoon coconut oil"
+                },
+                {
+                    "Text": "1 tablespoon vanilla"
+                },
+                {
+                    "Text": "1 teaspoon baking powder"
+                },
+                {
+                    "Text": "1/2 teaspoon baking soda"
+                },
+                {
+                    "Text": "1/2 teaspoon salt"
+                },
+                {
+                    "Text": "1 1/2 cups flour"
+                },
+                {
+                    "Text": "1/2 teaspoon red food coloring (optional)"
+                },
+                {
+                    "Text": "1/2 cup flaked baby coconut"
+                },
+                {
+                    "Text": "1 cup fresh cherries, pitted and chopped"
+                }
+            ],
+            "Instructions": [
+                {
+                    "Step": 1,
+                    "Text": "Preheat oven to 350F."
+                },
+                {
+                    "Step": 2,
+                    "Text": "Line a muffin pan with liners."
+                },
+                {
+                    "Step": 3,
+                    "Text": "Butter and line with parchment a 9 inch round."
+                },
+                {
+                    "Step": 4,
+                    "Text": "Cream the butter, coconut oil and the sugar, beat for about five minutes until light and fluffy."
+                },
+                {
+                    "Step": 5,
+                    "Text": "Add eggs, one at a time, until fully incorporated."
+                },
+                {
+                    "Step": 6,
+                    "Text": "Add vanilla extract and red food coloring."
+                },
+                {
+                    "Step": 7,
+                    "Text": "Combine dry ingredients and add the dry mixture into the butter mixture very slowly (otherwise you will get a face full of flour)."
+                },
+                {
+                    "Step": 8,
+                    "Text": "Add cherries and coconut, just mix until combined."
+                }
+            ],
+            "LargeImage": "images/cupcakes_128.png",
+            "Name": "Cup Cakes",
+            "NutritionalFacts": [
+                {
+                    "Name": "calories",
+                    "Text": "240 calories"
+                },
+                {
+                    "Name": "fatContent",
+                    "Text": "9g fat"
+                }
+            ],
+            "PrepTime": "30 minutes",
+            "Thumbnail": "images/cupcakes_48.png",
+            "Yield": "24 cupcakes"
+        }];
+});
